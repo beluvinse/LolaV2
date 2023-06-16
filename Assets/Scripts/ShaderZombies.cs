7a0628a0cfbd63858c52a92e457e5d8940d8b27c
beluvinse <83471976+beluvinse@users.noreply.github.com>
2023-06-16 10:34:29 -0300

    shaderssss

--- Assets/Scripts/ShaderZombies.cs
+++ Assets/Scripts/ShaderZombies.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class ShaderZombies : MonoBehaviour
 {
-    [SerializeField] private Material _effectMat;
+    [SerializeField] private Material _zombie1Mat;
+    [SerializeField] private Material _zombie2Mat;
+    [SerializeField] private Material _zombie3Mat;
+    [SerializeField] private Material _floorMat;
 
     void Update()
     {
-        _effectMat.SetVector("_PlayerPosition", transform.position);
+        _zombie1Mat.SetVector("_PlayerPosition", transform.position);
+        _zombie2Mat.SetVector("_PlayerPosition", transform.position);
+        _zombie3Mat.SetVector("_PlayerPosition", transform.position);
+        _floorMat.SetVector("_PlayerPosition", transform.position);
     }
 }
