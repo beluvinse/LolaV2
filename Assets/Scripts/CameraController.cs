bcf6d4334d538f3c624c86a544fd675c84c42885
beluvinse <83471976+beluvinse@users.noreply.github.com>
2023-06-29 19:17:34 -0300

    shader wall + nav mesh baking

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,56 +10,11 @@ public class CameraController : MonoBehaviour
 
     public Transform target;
 
-    private TransparentWall currentTransparentWall;
-
-
-
     private void FixedUpdate()
     {
         //transform.position = new Vector3(target.position.x, 22f, target.position.z -12f);
         transform.position = new Vector3(Mathf.Clamp(target.position.x, minPos.x, maxPos.x), 22f, Mathf.Clamp(target.position.z -12f, minPos.z, maxPos.z));
-        CheckPlayerBehindWall();
     }
-
-    private void CheckPlayerBehindWall()
-    {
-        Vector3 direction = target.position - transform.position;
-        //Calculate the length
-        float length = Vector3.Distance(target.position, transform.position);
-        //Draw the ray in the debug
-        Debug.DrawRay(transform.position, direction.normalized * length, Color.red);
-
-        RaycastHit currentHit;
-        //Cast the ray and report the firt object hit filtering by "Wall" layer mask
-        if (Physics.Raycast(transform.position, direction, out currentHit, length, LayerMask.GetMask("Wall")))
-        {
-            //Getting the script to change transparency of the hit object
-            TransparentWall transparentWall = currentHit.transform.GetComponent<TransparentWall>();
-            //If the object is not null
-            if (transparentWall)
-            {
-                //If there is a previous wall hit and it's different from this one
-                if (currentTransparentWall && currentTransparentWall.gameObject != transparentWall.gameObject)
-                {
-                    //Restore its transparency setting it not transparent
-                    currentTransparentWall.ChangeTransparency(false);
-                }
-                //Change the object transparency in transparent.
-                transparentWall.ChangeTransparency(true);
-                currentTransparentWall = transparentWall;
-            }
-        }
-        else
-        {
-            //If nothing is hit and there is a previous object hit
-            if (currentTransparentWall)
-            {
-                //Restore its transparency setting it not transparent
-                currentTransparentWall.ChangeTransparency(false);
-            }
-        }
-    }
-
 
     #region "Deprecated"
     private void Move(float xAxis, float zAxis)
